39f243a8015b18407979b9df99e3d191ef545efd
Iman Gure <imangu99@gmail.com>
2019-07-03 11:27:09 -0700

    deploy on heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-var PORT = 3000;
+//var PORT = 3000;
 
-app PORT = 3000;
+var PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
-pp.listen(PORT, function () {
+app.listen(PORT, function () {
     console.log(" listening on PORT " + PORT);
 });
 
